8b2ab1499890f994523c584d492e85c96c90a6e9
verdox <mail.ysp@web.de>
2024-12-27 22:34:59 +0000

    1.21.3 Fix patches and remove old patches.

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,6 +111,16 @@ publishing {
             }
         }
     }
+    repositories {
+        maven {
+            name = "verdox"
+            url = uri("https://repo.verdox.de/snapshots")
+            credentials {
+                username = (findProperty("reposilite.verdox.user") ?: System.getenv("REPO_USER")).toString()
+                password = (findProperty("reposilite.verdox.key") ?: System.getenv("REPO_PASSWORD")).toString()
+            }
+        }
+    }
 }
 allprojects {
     // Publishing API:
